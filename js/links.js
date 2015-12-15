0ce9504ee14c8daae362ec1a84f21064c4ad9007
Jeremy <y2kfirzen@gmail.com>
2015-12-14 19:55:29 -0800

    Set up a skeletal JSON structure for icon links. TODO: Decide on an icon pack

--- js/links.js
+++ js/links.js
@@ -1,3 +1,27 @@
 // Link Injection for Home Sweet Homepage
 // All links stored as JSON
 // TODO: Make link editing user-friendly.
+
+// Icons: 'Font Awesome', 'Material Icons'
+
+icon_links = [
+    {
+        "name":"Facebook",
+        "url":"https://www.facebook.com/",
+        "icon":"facebook"
+    },
+
+    {
+        "name":"Twitter",
+        "url":"https://www.twitter.com",
+        "icon":"twitter"
+    },
+
+    {
+        "name":"Messenger",
+        "url":"https://www.messenger.com",
+        // MTL icon pack
+        "icon":"message"
+    }
+];
+
