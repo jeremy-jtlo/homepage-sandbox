1b4712742478d623a42eec285f919a6f6c210d8c
Jeremy <y2kfirzen@gmail.com>
2018-11-10 14:29:37 -0800

    Added a link object and spawned a UL from it

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,48 @@
 // Tutorial on how to use nextjs
 // https://flaviocopes.com/nextjs/
 
+/**
+ * TODO: Find a css framework that I actually want to use
+ * (Let's try to move away from bootstrap/foundation)
+ * https://semantic-ui.com/
+ * https://bulma.io/
+ * http://yeticss.com/
+ * 
+ * Inspo resources:
+    >http://startpages.github.io/
+    >https://www.ricing.chloechantelle.com/
+    >http://nanami-tan.info/#HTMLPage
+    >http://pastebin.com/ZJvDn0eN
+ */
+
 import Link from 'next/link'
 import Head from 'next/head'
 
-const HeaderDrop = (props) => (
+var mylinks = {
+    "titleDest": "http://google.ca",
+    "title": "This is my list",
+    "sites": [
+        {"dest": "http://reddit.com", "key": "reddit"},
+        {"dest": "http://reddit.com/r/kappa", "key": "Kappa"},
+        {"dest": "http://reddit.com/r/monsterhunter", "key": "MonsterHunter"},
+        {"dest": "http://reddit.com/r/salty", "key": "Salty"},
+    ]
+};
+
+const HeaderDrop = (props) => {
+    var linkObjects = [];
+
+    props.links.sites.forEach(element => {
+        linkObjects.push(<Link href={element.dest}><a>{element.key}</a></Link>);
+    });
+
+    return(
     <ul>
         <a href={props.titleDest}>{props.title}</a>
+        {linkObjects}
     </ul>
-)
+    );
+}
 
 export default () => (
 <div>
@@ -18,6 +52,6 @@ export default () => (
     </Head>
     <h1>H E L L O  W O R L D</h1>
     <Link href="https://duckduckgo.com"><a>Test link component</a></Link>
-    <HeaderDrop titleDest="https://google.ca" title="Display String for UL"/>
+    <HeaderDrop titleDest="https://google.ca" title="Display String for UL" links={mylinks}/>
 </div>
 )
