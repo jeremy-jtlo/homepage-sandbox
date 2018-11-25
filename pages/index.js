5c281d9c451bb31bf5c088a545ec947246a86d8c
Jeremy <y2kfirzen@gmail.com>
2018-11-24 16:55:57 -0800

    Added ESLint to local workspace and silenced most of the warnings

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@
  * https://semantic-ui.com/
  * https://bulma.io/
  * http://yeticss.com/
- * 
+ *
  * Inspo resources:
     >http://startpages.github.io/
     >https://www.ricing.chloechantelle.com/
@@ -15,43 +15,30 @@
     >http://pastebin.com/ZJvDn0eN
  */
 
-import Link from 'next/link'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
 
-var mylinks = {
-    "titleDest": "http://google.ca",
-    "title": "This is my list",
-    "sites": [
-        {"dest": "http://reddit.com", "key": "reddit"},
-        {"dest": "http://reddit.com/r/kappa", "key": "Kappa"},
-        {"dest": "http://reddit.com/r/monsterhunter", "key": "MonsterHunter"},
-        {"dest": "http://reddit.com/r/salty", "key": "Salty"},
-    ]
+const mylinks = {
+  titleDest: 'http://google.ca',
+  title: 'This is my list',
+  sites: [
+    { dest: 'http://reddit.com', key: 'reddit' },
+    { dest: 'http://reddit.com/r/kappa', key: 'Kappa' },
+    { dest: 'http://reddit.com/r/monsterhunter', key: 'MonsterHunter' },
+    { dest: 'http://reddit.com/r/salty', key: 'Salty' },
+  ],
 };
 
-const HeaderDrop = (props) => {
-    var linkObjects = [];
-
-    props.links.sites.forEach(element => {
-        linkObjects.push(<Link href={element.dest}><a>{element.key}</a></Link>);
-    });
-
-    return(
-    <ul>
-        <a href={props.titleDest}>{props.title}</a>
-        {linkObjects}
-    </ul>
-    );
-}
-
-export default () => (
-<div>
-    <Head>
+export default function Home() {
+  const testlink1 = 'https://duckduckgo.com';
+  return (
+    <div>
+      <Head>
         <title>Home Sweet Home(page)</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <h1>H E L L O  W O R L D</h1>
-    <Link href="https://duckduckgo.com"><a>Test link component</a></Link>
-    <HeaderDrop titleDest="https://google.ca" title="Display String for UL" links={mylinks}/>
-</div>
-)
+      </Head>
+      <h1>H E L L O  W O R L D</h1>
+      <a href={testlink1}>Test link component</a>
+    </div>
+  );
+}
