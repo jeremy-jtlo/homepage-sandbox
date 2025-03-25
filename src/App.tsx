c276d161ccbf56bfbc7623b31d1b7f9c7ce1b3f7
Jeremy <y2kfirzen@gmail.com>
2025-03-25 11:28:20 -0700

    Update semantic to v3 beta2 to fix findDOMNode build break

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import { apps, newsLinks, redditLinks } from "./constants/links";
+import { Container, Grid, Header } from "semantic-ui-react";
+import ListLinks from "./components/listLinks";
+import SubHeader from "./components/subheader";
+import AppGrid from "./components/appGrid";
 import "./App.css";
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <p>here is the first app: {apps[0][0].name}</p>
-      <p>here is the first news: {newsLinks.sites[0].key}</p>
-      <p>here is the first reddit: {redditLinks.sites[0].key}</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <SubHeader time={new Date()} />
   </div>
 );
 
