d804198451d81bfd533a460583577beb144ad680
Jeremy <y2kfirzen@gmail.com>
2025-03-25 11:06:08 -0700

    Added list link render component for news and reddit

---
+++ src/components/listLinks.tsx
@@ -0,0 +1,30 @@
+import { List, Card } from "semantic-ui-react";
+import { siteLink } from "../constants/links";
+import React from "react";
+
+export type ListLinksProps = {
+  links: siteLink[];
+  title: string;
+  titleDest: string;
+};
+
+export default function ListLinks({ links, title, titleDest }: ListLinksProps) {
+  return (
+    <Card>
+      <Card.Header>
+        <a href={titleDest}>{title}</a>
+      </Card.Header>
+      <List divided relaxed>
+        {links.map((link) => (
+          <List.Item key={link.key}>
+            <List.Content>
+              <List.Description as="a" target="blank" href={link.dest}>
+                {link.key}
+              </List.Description>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
+    </Card>
+  );
+}
