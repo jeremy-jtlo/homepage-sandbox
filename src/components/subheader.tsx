9fd96c0d1336968404ddbf68f98d6c3995d7e393
Jeremy <y2kfirzen@gmail.com>
2025-03-25 10:35:39 -0700

    Apply prettier formatting, add stub data

---
+++ src/components/subheader.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+export type SubHeaderProps = { time: Date };
+
+export default function SubHeader({ time }: SubHeaderProps) {}
